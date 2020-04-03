78f07e0df72a465a99cac7df98758561493105b1
jenipha <jenipharachel@gmail.com>
2020-04-03 20:45:59 +0530

    changed blog and portfolio content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
 10.times do |blog|
   Blog.create!(
     title: "Blog Post #{blog}",
-    body: 'La fête nationale française s’appelle le Quatorze Juillet – et non pas « le jour de la Bastille » comme en anglais. Elle commémore la prise de cette fameuse prison le 14 juillet 1789 et le déclenchement de la Révolution française. Symbole du pouvoir absolu et arbitraire de l’Ancien Régime de Louis XVI, la conquête de la Bastille par le peuple prouva que le pouvoir du roi n’était plus absolu : les Français exigèrent qu’il se fonde sur la Nation et qu’il soit modéré par une séparation des pouvoirs.'
+    body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
   )
 end
 
@@ -22,7 +20,7 @@ puts '5 skills created'
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
     subtitle: 'My great service',
-    body: 'Ce ne fut qu’en 1880, alors que la France était définitivement devenue républicaine, que le député Benjamin Raspail déposa la loi déclarant le 14 juillet fête nationale. La signification de la chute de la Bastille est si forte, donc, parce qu’elle équivaut à la naissance de la République.',
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: 'https://via.placeholder.com/600X400',
     thumb_image: 'https://via.placeholder.com/350X200'
   )
