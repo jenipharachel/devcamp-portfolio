f56ec401881dfc2bf1112b47163515d5835efe83
jenipha <jenipharachel@gmail.com>
2020-04-07 00:53:57 +0530

    Implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ApplicationRecord
+    #validation of data for entry into db
     validates_presence_of :title, :body, :main_image, :thumb_image
+
+    #custom scopes -> used for querying data in db
+    def self.ruby_on_rails
+        where(subtitle: "Ruby on Rails")
+    end
+
+    scope :react_js, -> {where(subtitle: "React JS")}
 end
