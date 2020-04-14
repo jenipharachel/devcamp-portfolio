13a2236dfe56e64737fcb1cc65ea0ab7f8346851
jenipha <jenipharachel@gmail.com>
2020-04-14 22:13:33 +0530

    Rectified blog creation error

--- db/migrate/20200406170403_add_topic_reference_to_blogs.rb
+++ db/migrate/20200406170403_add_topic_reference_to_blogs.rb
@@ -1,5 +1,7 @@
 class AddTopicReferenceToBlogs < ActiveRecord::Migration[5.2]
   def change
     add_reference :blogs, :topic, foreign_key: true
+    #to avoid topic must exist error while creating a new blog
+    #add_column :blogs, :topic_id, :integer
   end
 end
